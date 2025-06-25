ee37b83b29bfb6eeb6acca0dc7ee844be80516f8
Anish2076 <anishjogi10@gmail.com>
2025-06-26 00:18:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// linguist-language=JavaScript
+// Music Player Script
+console.log("Player initialized");
+const audioPlayer = document.getElementById('player');
+// Add player functionality here
+function playTrack() {}
+function pauseTrack() {}
